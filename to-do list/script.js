d77db0a68547a0503385a234445a5d1490d29eb9
Rishabhuniyal22 <rishabhuniyal45@gmail.com>
2025-12-30 00:37:02 +0530

    Add files via upload

---
+++ to-do list/script.js
@@ -0,0 +1,26 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let taskText = input.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = `
+        <span onclick="toggleTask(this)">${taskText}</span>
+        <button class="delete-btn" onclick="deleteTask(this)">X</button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
+
+function deleteTask(btn) {
+    btn.parentElement.remove();
+}
+
+function toggleTask(text) {
+    text.parentElement.classList.toggle("completed");
+}
